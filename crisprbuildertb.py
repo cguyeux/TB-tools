d378746a0a73d4e1d054495eaedecb49da2fb78b
Christophe Guyeux <christophe.guyeux@univ-fcomte.fr>
2020-06-17 15:22:47 +0200

    initial import for CRISPRbuilder-TB

---
+++ crisprbuildertb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 15:21:13 2020
+
+@authors: Christophe Guyeux, Guislaine Refrégier, Christophe Sola
+"""
+
+class CRISPRbuilderTB:
+    pass
+
+
+if __name__ == "__main__":
+    CRISPRbuilderTB()
